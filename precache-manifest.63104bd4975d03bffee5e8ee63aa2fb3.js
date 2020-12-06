1babf754d0f03d8324873668a55bc04efe65bf69
Anton Rusak <4231915+arusak@users.noreply.github.com>
2020-12-06 21:53:51 +0300

    Updates

---
+++ precache-manifest.63104bd4975d03bffee5e8ee63aa2fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986320d3e56f9a03caea4ede91bf0197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "092432e93c4653633b51",
+    "url": "/static/css/main.f3b8d24c.chunk.css"
+  },
+  {
+    "revision": "7ce7ce49b85d0c6c6bc2",
+    "url": "/static/js/2.4c03d3b2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4c03d3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092432e93c4653633b51",
+    "url": "/static/js/main.839c311d.chunk.js"
+  },
+  {
+    "revision": "3049d38e4107ed77c1d4",
+    "url": "/static/js/runtime-main.f3ba14de.js"
+  }
+]);
